7392695bbfc8e4d7b8c2cc37d7d7ef6a43eb8393
William Maroney <32745456+WMaroney@users.noreply.github.com>
2018-10-22 14:12:29 -0400

    Update lotr_schema.sql
    Added faculty table and changed tables and columns/attributes

--- SQL/lotr_schema.sql
+++ SQL/lotr_schema.sql
@@ -1,10 +1,20 @@
-drop table if exists sponsor;
-create table sponsor (
-	internship_id integer primary key autoincrement,
-	sponsor_id integer not null,
+drop table if exists sponsors;
+create table sponsors (
+	internship_id integer foreign key,
+	sponsor_id integer primary key autoincrement,
 	sponsor_name text not null,
 	sponsor_companyname text not null,
 	sponsor_email text not null,
+	sponsor_internshiptitle text not null,
+	sponsor_dateavail text not null,
+	sponsor_special text not null,
+	student_password varchar not null
+);
+
+drop table if exists interships;
+create table internships (
+	internship_id integer primary key autoincrement,
+	sponsor_id integer foreign key,
 	sponsor_internshiptitle text not null,
 	sponsor_dateavail text not null,
 	sponsor_special text not null
@@ -18,16 +28,18 @@ create table students (
 	student_year text not null,
 	student_majorminor text not null,
 	student_gpa float not null,
-	student_skillshobbies text not null
+	student_skillshobbies text not null,
+	student_password varchar not null
 );
 
 drop table if exists applications;
 create table applications (
+	application_id integer autoincrement primary key,
 	internship_id integer foreign key,
 	student_id integer foreign key,
 	sponsor_companyname text foreign key not null,
 	sponsor_internshiptitle text foreign key not null,
-	date_avail integer not null,
+	date_avail integer not null
 );
 
 drop table if exists notifications;
@@ -37,6 +49,13 @@ create table notifications (
 	sender text not null,
 	recipient text not null,
 	description text not null,
-	read_unread text not null,
+	read_unread text not null
 );
 
+drop table if exists users;
+create table faculty (
+	faculty_id integer primary key autoincrement,
+	faculty_name text not null,
+	faculty_email text not null,
+	faculty_password text not null
+);
