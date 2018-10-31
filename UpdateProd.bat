4bf300ff413480b692c7c9b89dc6222a3458030b
Eric Brown <eric.david.brown@gmail.com>
2018-10-31 14:56:58 -0400

    Testing

---
+++ UpdateProd.bat
@@ -0,0 +1,3 @@
+git pull https://github.com/ISTEricBrown/InternSearch
+git remote add upstream https://github.com/scsu-csc330-400/a-team-prod
+git push https://github.com/scsu-csc330-400/a-team-prod -f
